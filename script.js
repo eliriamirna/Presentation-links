20b945ee91873ee3b34be07559cc2d0b6e3c244d
Eliria Dias <eliriadias@gmail.com>
2024-01-31 14:08:25 -0300

    primeiro commit

---
+++ script.js
@@ -0,0 +1,13 @@
+function toggleMode() {
+  const html = document.documentElement
+
+  html.classList.toggle('light')
+
+  // const img = document.querySelector("#profile img")
+
+  // if(html.classList.contains('light')) {
+  //   img.setAttribute("alt", "foto de Elíria usndo tranças jaqueta jeans sorrindo com uma mão no queixo")
+  // } else {
+  //    img.setAttribute("alt", "se tivesse outra foto pro mode dark")
+  // }
+}
